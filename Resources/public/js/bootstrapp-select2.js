14c61fa1bfd600cb78b03fc8cec29b3b14bd62b8
Nathanaël Mariani <github@nmariani.fr>
2013-10-25 11:34:04 +0200

    Add Select2 tags support using select in bootstrappSelect2

--- Resources/public/js/bootstrapp-select2.js
+++ Resources/public/js/bootstrapp-select2.js
@@ -7,13 +7,20 @@ $.fn.bootstrappSelect2 = function(options) {
             var choices = element.data('choices');
             if (typeof choices != 'undefined') {
                 opts.data = choices;
-                var id = element.attr('id'),
-                    name = element.attr('name'),
-                    value = element.val();
-                element.replaceWith(
-                    $('<input type="hidden" id="' + id + '" name="'+ name +'" value="'+ value +'">')
-                );
-                element = $('#'+id);
+            }
+            switch (true) {
+                case typeof(opts.data) != 'undefined' :
+                case typeof(opts.tags) != 'undefined' :
+                    var id = element.attr('id'),
+                        name = element.attr('name'),
+                        value = element.val();
+                    element.replaceWith(
+                        $('<input type="hidden" id="' + id + '" name="'+ name +'" value="'+ value +'">')
+                    );
+                    element = $('#'+id);
+                    break;
+                default :
+                    break;
             }
         }
         select2 = element.select2(opts);
